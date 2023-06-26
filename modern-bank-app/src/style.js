d5ded258c4b10d2de1d52f17d13ea1f57f558131
Hexcss <cadersuay.javier@gmail.com>
2023-06-26 16:34:49 +0200

    feat: Hero

--- modern-bank-app/src/style.js
+++ modern-bank-app/src/style.js
@@ -13,6 +13,13 @@ const styles = {
 
   marginX: "sm:mx-16 mx-6",
   marginY: "sm:my-16 my-6",
+
+  headerOne: "flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]",
+  headerTwo: "font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full",
+
+  pinkGradient: "absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient",
+  whiteGradient: "absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient",
+  blueGradient: "absolute z-[0] w-[50%] h-[45%] right-20 bottom-20 blue__gradient",
 };
 
 export const layout = {
